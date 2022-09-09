93222d196e36a66beef96fdfa34e7358c665e130
cop <32881179+antinuke0day@users.noreply.github.com>
2022-09-09 14:49:46 -0400

    Update comment.py

--- TikTok-Async/comment.py
+++ TikTok-Async/comment.py
@@ -6,28 +6,28 @@ class Comment:
     def __init__(self, api):
         self.api = api
     
-    def get_comments(self, video_id, cursor="0", count="20", region="US", raw=False):
+    async def get_comments(self, video_id, cursor="0", count="20", region="US", raw=False):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://api2-19-h2.musical.ly/aweme/v1/comment/list/?aweme_id={video_id}&cursor={cursor}&count={count}&comment_style=2&device_type=SM-G973N&region={region}") as comments:
                 res = await comments.json()
-        return self.format_comments(res["comments"], raw)
+        return await self.format_comments(res["comments"], raw)
 
     async def get_replies(self, comment_id, count="20", cursor="0", raw=False):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://api2-19-h2.musical.ly/aweme/v1/comment/list/reply/?comment_id={comment_id}&cursor={cursor}&count={count}&comment_style=2&device_type=SM-G973N&media_type=4") as replies:
                 res = await replies.json()
-        return self.format_comments(res["comments"], raw=raw)
+        return await self.format_comments(res["comments"], raw=raw)
 
-    def format_comments(self, comments_data, raw=False, replies=False):
+    async def format_comments(self, comments_data, raw=False, replies=False):
         if raw:
             return comments_data
         comments = []
         for comment in comments_data:
-            data = self.parse_comment_data(comment, replies)
+            data = await self.parse_comment_data(comment, replies)
             comments.append(data)
         return comments
 
-    def parse_comment_data(self, comment, replies=False):
+    async def parse_comment_data(self, comment, replies=False):
         data = {}
         commenter = comment["user"]
         data["comment_id"] = comment["cid"]
@@ -39,7 +39,7 @@ class Comment:
             data["total_replies"] = comment["reply_comment_total"]
         data["replied_to_id"] = comment["reply_to_reply_id"]
         if replies:
-            data["replies"] = self.get_replies(comment["cid"]) # might be empty might not be, if empty then request the replies by comment.get_replies
+            data["replies"] = await self.get_replies(comment["cid"]) # might be empty might not be, if empty then request the replies by comment.get_replies
         data["commenter"] = {
             "nickname": commenter["nickname"],
             "user_id": commenter["uid"],
