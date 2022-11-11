85d583c53a93738b662a2d7fbf93a7b9532e12be
cop <32881179+antinuke0day@users.noreply.github.com>
2022-11-11 03:08:27 -0500

    Delete encryption directory

--- encryption/tiktok.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def rticket():
-    ticket = str(time.time() * 1000).split(".")[0]
-    return ticket
-
-def ts():
-    timestamp = str(time.time()).split(".")[0]
-    return timestamp
