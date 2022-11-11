237a9e1afc49ac1d421c361689e8d6a6008196d6
cop <32881179+antinuke0day@users.noreply.github.com>
2022-11-11 03:01:59 -0500

    Update video.py

--- video.py
+++ video.py
@@ -1,44 +1,63 @@
 from datetime import datetime
 import re,aiohttp,orjson
+from pydantic import AnyUrl, BaseModel, Field
+from typing import Optional
 
-async def get_video_binary(download_url):
-    """
-    DOWNLOAD_URL (str):
-        Get this from the object that the parse_video_data function returns, it can either be download_video_url or download_video_url_watermark
-        
-    Returns:
-        binary: Raw binary mp4 data        
-    """
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(download_url) as video:
-                binary=await video.read()
-        #self.api.debug.success(f"Received binary data ({video.elapsed.total_seconds()}s)")
-        return binary
-    except Exception as e:
-        print(e)
-        
-async def get_data(url, raw=False) -> dict:
-    """Grabs the video data from a tiktok video url
-    
-    URL/VIDEO_ID (str):
-        https://vm.tiktok.com/ZMNnX3Q4q 
-        7116227445648395526 
-        https://www.tiktok.com/@peachyfitness4/video/7116227445648395526
-    
-    RAW (bool):
-        Optional if u want the raw data tiktok provided from the video (this contains way more info)
-        
-    Returns:
-        formatted data from the video in a json object 
-        
-    """
-    async with aiohttp.ClientSession(json_serialize=orjson.loads) as session:
-        async with session.get(f"https://tikwm.com/api?url={url}") as f:
-            d=await f.json()
-            return d['data']
+class MusicInfo(BaseModel):
+    id: Optional[int] = Field(None, title='Music ID')
+    title: Optional[str] = Field(None, title='Music Title')
+    play: Optional[str] = Field(None, title='Music Play URL')
+    cover: Optional[str] = Field(None, title='Cover URL')
+    author: Optional[str] = Field(None, title='Sound Author')
+    original: Optional[bool] = Field(None, title='If Original Or Not')
+    duration: Optional[int] = Field(None, title='Sound Duration')
+    album: Optional[str] = Field(None, title='Sound Album')
 
-def highest_soundquality_download_url(data):
-    bit_rates = data["bit_rate"]
-    bit_rates.sort(key=lambda key: key["bit_rate"], reverse=True)
-    return bit_rates[0]["play_addr"]["url_list"][2]
+class Author(BaseModel):
+    id: Optional[int] = Field(None, title='Author ID')
+    unique_id: Optional[str] = Field(None, title='Author Username')
+    nickname: Optional[str] = Field(None, title='Author Nickname')
+    avatar: Optional[str] = Field(None, title='Author Avatar')
+
+
+class Statistics(BaseModel):
+    id: Optional[int] = Field(None, title='Video ID')
+    region: Optional[str] = Field(None, title='Video Region')
+    title: Optional[str] = Field(None, title='Video Title')
+    cover: Optional[str] = Field(None, title='Cover Image')
+    origin_cover: Optional[str] = Field(None, title='Origin Cover')
+    duration: Optional[int] = Field(None, title='Video Duration')
+    play: Optional[str] = Field(None, title='Non Watermarked URL')
+    wmplay: Optional[str] = Field(None, title='Watermarked URL')
+    music: Optional[str] = FIeld(None, title='Sound URL')
+    music_info: Optional[MusicInfo] = None
+    play_count: Optional[int] = Field(None, title='Sound Play Count')
+    digg_count: Optional[int] = Field(None, title='Like Count')
+    comment_count: Optional[int] = Field(None, title='Comment Count')
+    share_count: Optional[int] = Field(None, title='Share Count')
+    download_count: Optional[int] = Field(None, title='Download Count')
+    create_time: Optional[int] = Field(None, title='Creation TimeStamp')
+    author: Optional[Author] = None
+
+
+class TikTok(BaseModel):
+    code: Optional[int] = Field(None, title="Status Code")
+    msg: Optional[str] = Field(None, title="Status Message")
+    processed_time: Optional[float] = Field(None, title='Total Time Taken to Process')
+    data: Optional[Statistics] = None
+
+
+    async def get_video_binary(self,download_url):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.data.play) as video:
+                    binary=await video.read()
+            return binary
+        except Exception as e:
+            print(e)
+            
+    @classmethod
+    async def get_data(self,url):
+        async with aiohttp.ClientSession(json_serialize=orjson.loads) as session:
+            async with session.get(f"https://tikwm.com/api?url={url}") as f:
+                return cls.parse_raw(await f.read())
